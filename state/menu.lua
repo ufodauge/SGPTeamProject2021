9adc578788f82802ec5a46af35170076edadd5a3
agdfydy <hakka.ydy.sub@gmail.com>
2021-03-15 02:17:39 +0900

    ひとまず通常面は終わり

---
+++ state/menu.lua
@@ -0,0 +1,21 @@
+local menu = {}
+
+menu.name = 'menu'
+
+function menu:init()
+
+end
+
+function menu:enter()
+end
+
+function menu:update(dt)
+end
+
+function menu:draw()
+end
+
+function menu:leave()
+end
+
+return menu
